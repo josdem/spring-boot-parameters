e23186c89816aa4110aa602ab2ebb886ad9cb004
josdem <joseluis.delacruz@gmail.com>
2018-11-16 13:21:10 -0500

    Reading custom parameter from command line

--- src/main/java/com/jos/dem/springboot/parameter/ParameterApplication.java
+++ src/main/java/com/jos/dem/springboot/parameter/ParameterApplication.java
@@ -1,12 +1,26 @@
-package com.example.springbootparameters;
+package com.jos.dem.springboot.parameter;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ParameterApplication {
+
+  @Value("${user}")
+  private String user;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ParameterApplication.class, args);
 	}
+
+  @Bean
+  CommandLineRunner run(){
+    return args -> {
+      System.out.println("user: " + user);
+    };
+  }
+
 }
